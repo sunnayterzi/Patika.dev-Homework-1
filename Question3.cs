9e9869e29e33a3206d1ed6fcff718c42f409637d
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 15:42:14 +0300

    Add files via upload

---
+++ Question3.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine( n +" adet kelime giriniz");
+string [] arr = new string[n];
+for(int i = 0; i<arr.Length;i++){
+    arr[i]=Console.ReadLine();
+}
+Array.Reverse(arr);
+Console.WriteLine("Kelimelerin tersten sıralanışı");
+foreach(string i in arr){
+    Console.WriteLine(i);
+}
