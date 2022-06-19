330a883764c1f72a49401e1d5ca9fc547c00499a
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 15:54:08 +0300

    Add files via upload

---
+++ Question4.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir cümle giriniz");
+String n =Console.ReadLine();
+string [] arr = n.Split(" ");
+string.Join("",arr);
+Console.WriteLine(arr.Length+" adet kelime bulunmaktadır");
+
+n=n.Replace(" ","");
+Console.WriteLine("Toplam harf sayısı "+ n.Length);
