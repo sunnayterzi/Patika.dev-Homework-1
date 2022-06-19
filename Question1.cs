4c8781f54da56879177c132a224f84fb20b1bbd5
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 15:15:50 +0300

    Add files via upload

---
+++ Question1.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i<=n;i++){
+    if (i%2==0) Console.WriteLine(i);
+}
